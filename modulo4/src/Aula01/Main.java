6ca1692e15ca8d2dcf7bcaefde666c88996faec2
reotsugua <renanaugusto.ras@gmail.com>
2023-01-30 19:33:08 -0300

    Datas com Java - Parte I

---
+++ modulo4/src/Aula01/Main.java
@@ -0,0 +1,14 @@
+package Aula01;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        //LocalDate
+        //AAAA-MM-DD
+
+        LocalDate dataLocal = LocalDate.now();
+        System.out.println("dataLocal = " + dataLocal);
+    }
+}
