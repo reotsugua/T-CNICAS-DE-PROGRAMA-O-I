d6134386e4f959012d1f7c43e721455d4b65e3a2
reotsugua <renanaugusto.ras@gmail.com>
2023-02-09 22:16:41 -0300

    Aprendendo DateTimeFormatter

--- modulo4/src/Aula02/PracticeFormatDate.java
+++ modulo4/src/Aula02/PracticeFormatDate.java
@@ -1,5 +1,6 @@
 package Aula02;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -16,9 +17,22 @@ public class PracticeFormatDate {
         System.out.println(agora.format(DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm a")));
 
         System.out.println(agora.format(DateTimeFormatter.ofPattern("dd 'de' MMMM 'de' yyyy")));
-        System.out.println(agora.format(DateTimeFormatter.ofPattern("'São Paulo 'dd 'de' MMMM 'de' yyyy")));
+        System.out.println(agora.format(DateTimeFormatter.ofPattern("'São Paulo, 'dd 'de' MMMM 'de' yyyy")));
 
+        String dataString = "12/01/2023";
+        String dataTime = "12/02/2023 22:08";
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate data = LocalDate.parse(dataString, formatter);
 
+        DateTimeFormatter formatado = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        LocalDateTime time = LocalDateTime.parse(dataTime, formatado);
+
+        
+        
+        
+        System.out.println("dataFormatada = " + data);
+        System.out.println("time = " + time);
+        System.out.println(time.format(DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm a")));
     }
 }
