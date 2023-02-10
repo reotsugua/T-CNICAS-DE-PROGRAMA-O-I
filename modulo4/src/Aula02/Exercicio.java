99fce221da1a41be3793223e6bb758a1edb3442f
reotsugua <renanaugusto.ras@gmail.com>
2023-02-09 22:26:08 -0300

    Atualizando o exercicio da Vacina com DateTimeFormatter

--- modulo4/src/Aula02/Exercicio.java
+++ modulo4/src/Aula02/Exercicio.java
@@ -9,9 +9,17 @@ import java.time.temporal.ChronoUnit;
 
 public class Exercicio {
     public static void main(String[] args) {
-        LocalDate dataInicial = LocalDate.of(2023, 1, 8);
-        LocalDate dateFinal = LocalDate.of(2023, 4, 8);
+        /*LocalDate dataInicial = LocalDate.of(2023, 1, 8);
+        LocalDate dateFinal = LocalDate.of(2023, 4, 8);*/
+        String dataI = "09/01/2023";
+        String dataF = "09/04/2023";
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dataInicial = LocalDate.parse(dataI, formatter);
+        LocalDate dataFinal = LocalDate.parse(dataF, formatter);
+
         System.out.println("Data Inicial = " + dataInicial);
+        System.out.println("dataFinal = " + dataFinal);
 
         Period mes = Period.ofMonths(1);
         Period period = Period.of(0, 1, 5);
@@ -24,12 +32,13 @@ public class Exercicio {
         System.out.println("dataTerceiraDose = " + dataTerceiraDose);
 
         LocalDate dataDose = dataInicial;
-        while (dataDose.isBefore(dateFinal)) {
-            System.out.println("Dose tomada em: " + dataDose);
+        while (dataDose.isBefore(dataFinal)) {
+            System.out.println("Dose tomada em: " + dataDose.format(DateTimeFormatter.ofPattern("dd/MM/yyyy" + " EEEE")));
             dataDose = dataDose.plus(period);
         }
 
         LocalDate now = LocalDate.now();
+        //Aparecer dida da Semana
         System.out.println(now.format(DateTimeFormatter.ofPattern("EEEE")));
     }
 }
