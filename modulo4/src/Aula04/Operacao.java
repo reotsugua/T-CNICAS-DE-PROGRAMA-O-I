ae9b5797986952aef709a5b7ba4a54a62becd447
reotsugua <renanaugusto.ras@gmail.com>
2023-02-10 16:46:15 -0300

    Aula 04 Lambda - Calculadora

---
+++ modulo4/src/Aula04/Operacao.java
@@ -0,0 +1,5 @@
+package Aula04;
+
+public interface Operacao {
+    int executa(int a, int b);
+}
---
+++ modulo4/src/Aula04/ProgramacaoFuncional.java
@@ -0,0 +1,25 @@
+package Aula04;
+
+public class ProgramacaoFuncional {
+    public static void main(String[] args) {
+        System.out.println(soma(5,6));
+        System.out.println(subtracao(5,6));
+        
+        Operacao corpoDoMetodo = ((a, b) -> a+b);
+        Operacao sub = ((a, b) -> a-b);
+        System.out.println(calcula(5,6,corpoDoMetodo));
+        System.out.println(calcula(5,6,sub));
+    }
+    public static int soma (int a, int b) {
+        return a + b;
+    }
+    public static int subtracao (int a, int b) {
+        return a - b;
+    }
+
+    public static int calcula (int a, int b, Operacao operacao) {
+        return operacao.executa(a , b);
+    }
+
+
+}
