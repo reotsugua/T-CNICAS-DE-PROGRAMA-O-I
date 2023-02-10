dbcac25fc5cf584a6b7c4046b69dab574ef04cc4
reotsugua <renanaugusto.ras@gmail.com>
2023-02-10 16:22:37 -0300

    Aula 04 Lambda

---
+++ modulo4/src/Aula04/Main.java
@@ -0,0 +1,14 @@
+package Aula04;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(calcula(5,6));
+    }
+
+
+    public static int calcula (int a, int b) {
+        return a + b;
+    }
+
+
+}
