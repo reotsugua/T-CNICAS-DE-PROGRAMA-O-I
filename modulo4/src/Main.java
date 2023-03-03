0ac1ba77381ab9382686cc6b8d876f6cfb9a4636
reotsugua <renanaugusto.ras@gmail.com>
2023-03-03 19:20:30 -0300

    primeiro commit

--- modulo4/src/Main.java
+++ modulo4/src/Main.java
@@ -1,5 +1,6 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public abstract class X {...}
+public class Main{
+    public static void main (String[] args) {
+        X x = new X();
     }
 }
